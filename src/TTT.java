ea71f47c2268cc82b842857d8b73965435a91d9a
ha33ann <91528684+ha33ann@users.noreply.github.com>
2022-09-03 04:02:06 +0200

    Update TTT.java

--- src/TTT.java
+++ src/TTT.java
@@ -1,11 +1,28 @@
 import java.util.*;
+import java.util.Random;
 public class TTT {
 	    
 	    static String[] tile;
 	    static String turn;
 
-	    //checkWInner metoden kommer att kolla pÂ kombinationen av
-	    //tre svar given h‰r nere och best‰mma vinnaren.
+	  //Klassen restartGame d√§r logiken finns f√∂r att starta om spelet.
+	    public static void restartGame(String randomString, int length) {
+	    	Random random = new Random();
+	    	StringBuilder sb = new StringBuilder();
+	    	tile = new String[9];
+	    	for (int a = 0; a < 9; a++) {
+	            tile[a] = String.valueOf(a + 1);
+	        }
+	    	for(int i = 0; i < length; i++) {
+		          int index = random.nextInt(randomString.length());
+		          char randomChar = randomString.charAt(index);
+		          sb.append(randomChar);
+		        }
+	    	turn = sb.toString();
+	    }
+	    
+	    //checkWinner metoden kommer att kolla p√• kombinationen av
+	    //tre svar given h√§r nere och best√§mma vinnaren.
 	    static String checkWinner()
 	    {
 	        for (int a = 0; a < 8; a++) {
@@ -56,13 +73,13 @@ public class TTT {
 	                return "draw";
 	            }
 	        }
-	         // Metod fˆr att ange X eller O pÂ exakta tile.
+	         // Metod f√∂r att ange X eller O pÔøΩ exakta tile.
 	        System.out.println(
 	            turn + "'s turn; enter a tile number to place "
 	            + turn + " in:");
 	        return null;
 	    }
-	    //Printar ut spelbr‰dan
+	    //Printar ut spelbr√§dan
 	    static void BoardLayout()
 	    {
 	        System.out.println("-----------");
@@ -79,13 +96,42 @@ public class TTT {
 	                           + " ");
 	        System.out.println("-----------");
 	    }
-	  
+	    
 	    public static void main(String[] args)
 	    {
+
+	    	// Skapar en string av X och O
+	        String randomString = "XO";
+
+	        // Skapar en String builder
+	        StringBuilder sb = new StringBuilder();
+
+	        // Skapar en object av random klass
+	        Random random = new Random();
+
+	        // Anger l√§ngden p√• bokst√§ver
+	        int length = 1;
+
+	        for(int i = 0; i < length; i++) {
+
+	          // Genererar random index nummer
+	          int index = random.nextInt(randomString.length());
+
+	          // F√• fram bokstaven med hj√§lp av indexen p√• string
+	          char randomChar = randomString.charAt(index);
+
+	          // Appendar bokstaven till string builder
+	          sb.append(randomChar);
+	        }
+
+	        String finalString = sb.toString();
+
 	        Scanner in = new Scanner(System.in);
 	        tile = new String[9];
-	        turn = "X";
+	        turn = finalString;
 	        String winner = null;
+	        
+	        
 	  
 	        for (int a = 0; a < 9; a++) {
 	            tile[a] = String.valueOf(a + 1);
@@ -95,14 +141,14 @@ public class TTT {
 	        BoardLayout();
 	  
 	        System.out.println(
-	            "X's turn first. Enter a tile number to place X in:");
+	            finalString + "'s turn first. Enter a tile number to place X in:");
 	  
 	        while (winner == null) {
 	            int numInput;
 	            
 	            //Exeption handling.
-	            //numInput kommer att ta input frÂn spelaren, 1-9.
-	            //om det inte ‰r 1-9 sÂ kommer man att fÂ en error.
+	            //numInput kommer att ta input fr√•n spelaren, 1-9.
+	            //om det inte √§r 1-9 sÔøΩ kommer man att f√• en error.
 	            try {
 	                numInput = in.nextInt();
 	                if (!(numInput > 0 && numInput <= 9)) {
@@ -117,7 +163,7 @@ public class TTT {
 	                continue;
 	            }
 
-	            //H‰r ‰r logiken som best‰mmer vems tur det ‰r.
+	            //H√§r √§r logiken som best√§mmer vems tur det √§r.
 	            if (tile[numInput - 1].equals(
 	                    String.valueOf(numInput))) {
 	                tile[numInput - 1] = turn;
@@ -136,19 +182,39 @@ public class TTT {
 	                System.out.println(
 	                    "Tile already taken; please enter tile number:");
 	            }
-	        }
 	        
-	        //Om ingen vinner eller fˆrlorar sÂ blir det ett "draw".
-	        if (winner.equalsIgnoreCase("draw")) {
+	        //Om ingen vinner eller f√∂rlorar s√• blir det ett "draw" samt option f√∂r att restarta spelet.
+	        if (winner != null && winner.equalsIgnoreCase("draw")) {
 	            System.out.println(
 	                "It's a draw! Thanks for playing. :)");
+	            System.out.println("Enter restart to start a new game or quit to exit the game.");
+	            Scanner s = new Scanner(System.in);
+	            String choice = s.next();
+	            if(choice.equals("restart")) {
+	            	restartGame(randomString, length);
+	            	winner = null;
+	            	checkWinner();
+	            	BoardLayout();
+	            }
 	        }
 
-	        //Annars sÂ visas vinnaren + winner meddelande.
-	        else {
+	        //Annars s√• visas vinnaren + winner meddelande same option att starta om spelet.
+	        else if (winner != null ){
 	            System.out.println(
 	                "Congratulations! " + winner
 	                + " have won! Thanks for playing. :)");
+	            System.out.println("Enter restart to start a new game or quit to exit the game.");
+	            Scanner s = new Scanner(System.in);
+	            String choice = s.next();
+	            if(choice.equals("restart")) {
+	            	restartGame(randomString, length);
+	            	winner = null;
+	            	checkWinner();
+	            	BoardLayout();
+	            }
 	        }
+	        
+	            
 	    }
-	}
+	    }
+}
